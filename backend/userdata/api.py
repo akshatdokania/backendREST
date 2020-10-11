2e624e1c1d1d812ed9c7a2c57e157c5438476e42
akshatdokania <akshatdokania@users.noreply.github.com>
2020-10-11 14:14:21 +0530

    corrected indents

--- backend/userdata/api.py
+++ backend/userdata/api.py
@@ -45,63 +45,63 @@ class UserDetail(APIView):
 
 
     def checkparams(params):
-    total = 0
-    total_param = 21
-    count_epi = 0
-    count_vital = 0
-    count_lab = 0
-    riskMessage = "Risk returned null"
-    if int(params['age']) > 55:
-        counter += 1
-    if 'hpd' in params:
-        count_epi += 1
-    if 'ckd' in params:
-        count_epi += 1
-    if 'dm' in params:
-        count_epi += 1
-    if 'htn' in params:
-        count_epi += 1
-    if 'hiv' in params:
-        count_epi += 1
-    if 'trans' in params:
-        count_epi +=  1
-    if int(params['resrate']) > 24:
-        count_vital += 1
-    if int(params['heartrate']) > 125:
-        count_vital += 1
-    if int(params['spo']) < 90:
-        count_vital += 1
-    if int(params['ddimer']) != None and int(params['ddimer']) > 1000:
-        count_lab += 1
-    if int(params['cpk']) != None and int(params['cpk']) > 400:
-        count_lab += 1
-    if int(params['crp']) != None and int(params['crp']) > 100:
-        count_lab += 1
-    if int(params['ldh']) != None and int(params['ldh']) > 245:
-        count_lab += 1
-    if float(params['tropo']) != None and float(params['tropo']) > 0.1:
-        count_lab += 1
-    if int(params['ferr']) != None and int(params['ferr']) > 500:
-        count_lab += 1
-    if float(params['absolute']) != None and float(params['absolute']) < 0.8:
-        count_lab += 1
-    total = count_epi+count_lab+count_vital
-    risk = (total/total_param) * 10
-    risk = round(risk, 2)
-    total_vl = count_lab+count_vital
-    if(count_epi>0 and count_vital>0 and count_lab>0):
-        riskMessage = "High Risk"
+        total = 0
+        total_param = 21
+        count_epi = 0
+        count_vital = 0
+        count_lab = 0
+        riskMessage = "Risk returned null"
+        if int(params['age']) > 55:
+            counter += 1
+        if 'hpd' in params:
+            count_epi += 1
+        if 'ckd' in params:
+            count_epi += 1
+        if 'dm' in params:
+            count_epi += 1
+        if 'htn' in params:
+            count_epi += 1
+        if 'hiv' in params:
+            count_epi += 1
+        if 'trans' in params:
+            count_epi +=  1
+        if int(params['resrate']) > 24:
+            count_vital += 1
+        if int(params['heartrate']) > 125:
+            count_vital += 1
+        if int(params['spo']) < 90:
+            count_vital += 1
+        if int(params['ddimer']) != None and int(params['ddimer']) > 1000:
+            count_lab += 1
+        if int(params['cpk']) != None and int(params['cpk']) > 400:
+            count_lab += 1
+        if int(params['crp']) != None and int(params['crp']) > 100:
+            count_lab += 1
+        if int(params['ldh']) != None and int(params['ldh']) > 245:
+            count_lab += 1
+        if float(params['tropo']) != None and float(params['tropo']) > 0.1:
+            count_lab += 1
+        if int(params['ferr']) != None and int(params['ferr']) > 500:
+            count_lab += 1
+        if float(params['absolute']) != None and float(params['absolute']) < 0.8:
+            count_lab += 1
+        total = count_epi+count_lab+count_vital
+        risk = (total/total_param) * 10
+        risk = round(risk, 2)
+        total_vl = count_lab+count_vital
+        if(count_epi>0 and count_vital>0 and count_lab>0):
+            riskMessage = "High Risk"
+            return count_epi,count_lab,count_vital,risk,riskMessage
+        elif(tot_vl == 2 and count_lab != 0 and count_vital != 0):
+            riskMessage = "Moderate Risk"
+            return count_epi,count_lab,count_vital,risk,riskMessage
+        elif(tot_vl >= 2):
+            riskMessage = "High Risk"
+            return count_epi,count_lab,count_vital,risk,riskMessage
+        elif(count_epidem > 0 and (count_lab == 1 or count_vital == 1)):
+            riskMessage = "Moderate Risk"
+            return count_epi,count_lab,count_vital,risk,riskMessage
+        elif(total < 2 or (count_epidem > 0 and count_lab == 0 and count_vital == 0)):
+            riskMessage = "Low Risk"
+            return count_epi,count_lab,count_vital,risk,riskMessage
         return count_epi,count_lab,count_vital,risk,riskMessage
-    elif(tot_vl == 2 and count_lab != 0 and count_vital != 0):
-        riskMessage = "Moderate Risk"
-        return count_epi,count_lab,count_vital,risk,riskMessage
-    elif(tot_vl >= 2):
-        riskMessage = "High Risk"
-        return count_epi,count_lab,count_vital,risk,riskMessage
-    elif(count_epidem > 0 and (count_lab == 1 or count_vital == 1)):
-        riskMessage = "Moderate Risk"
-        return count_epi,count_lab,count_vital,risk,riskMessage
-    elif(total < 2 or (count_epidem > 0 and count_lab == 0 and count_vital == 0)):
-        riskMessage = "Low Risk"
-        return count_epi,count_lab,count_vital,risk,riskMessage
-    return count_epi,count_lab,count_vital,risk,riskMessage
